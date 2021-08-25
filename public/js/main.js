664be9341a73da24b1b98f1e84dcbb0e6292fc29
Mathew Roichman <roichmankill@gmail.com>
2021-08-25 14:37:44 -0300

    Homepage concluída; links de contato e localização adicionados

--- public/js/main.js
+++ public/js/main.js
@@ -56,3 +56,33 @@ function viewMore(cardList, event) {
 	event.target.classList.remove('d-flex');
 	event.target.classList.add('d-none');
 }
+
+const comments = document.getElementsByClassName('comment');
+
+function createMoreBtn(clickHandler) {
+      let moreBtn = document.createElement('a');
+      moreBtn.innerHTML = '<br/>...ver mais';
+      moreBtn.classList.add('link', 'link-primary');
+      moreBtn.style.cursor = 'pointer';
+      if (moreBtn.addEventListener) {
+        moreBtn.addEventListener('click', clickHandler, false);
+      } else if (moreBtn.attachEvent) {
+        moreBtn.attachEvent('onclick', clickHandler);
+      } else {
+        moreBtn.onclick = clickHandler;
+      }
+      return moreBtn;
+    }
+
+for (let i in comments) {
+    let comment = new MultiClamp(document.querySelectorAll('.comment')[i], {
+      clamp: 4,
+      ellipsis: createMoreBtn(function() {
+        comment.reload({
+          clamp: 'auto',
+          useOriginalText: true
+        })
+      })
+    })
+}
+
